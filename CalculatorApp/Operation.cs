3d55a8a45aa8977ca4015b1141fb84c9e0fa843e
bartekmi <220bartek@gmail.com>
2017-12-08 17:33:50 -0700

    First upload

---
+++ CalculatorApp/Operation.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp {
+
+    public abstract class Operation {
+
+        public static List<Operation> ALL_OPERATIONS = new List<Operation>() {
+            new Addition(),
+            new Subtraction(),
+            new Multiplication(),
+            new Division(),
+        };
+
+        public int minInputsCount;
+        public int maxInputsCount;
+
+        public bool IsValid(int elementsCount) {
+            if (maxInputsCount <= elementsCount && elementsCount <= minInputsCount) {
+                return true;
+            }
+            else return false;
+        }
+
+        public abstract double Calculate(List<double> inputs);
+
+    }
+
+    public class Addition : Operation {
+        public Addition() {
+            minInputsCount = 1;
+            maxInputsCount = int.MaxValue;
+        }
+        public override double Calculate(List<double> inputs) {
+            return CalculateStatic(inputs);
+        }
+
+        public static double CalculateStatic(List<double> inputs) {
+            double solution = 0;
+            foreach(double element in inputs) {
+                solution = solution + element;
+            }
+            return solution;
+        }
+
+    }
+
+    public class Subtraction : Operation {
+        public Subtraction() {
+            minInputsCount = 1;
+            maxInputsCount = int.MaxValue;
+        }
+        public override double Calculate(List<double> inputs) {
+            int i = 0;
+            while(i < inputs.Count) {
+                if (i == 0) {
+                    //Don't flip first element to negative
+                } else {
+                    inputs[i] = inputs[i] * -1;
+                }
+                i = i + 1;
+            }
+            return Addition.CalculateStatic(inputs);
+        }
+    }
+
+    public class Multiplication : Operation {
+        public Multiplication() {
+            minInputsCount = 1;
+            maxInputsCount = int.MaxValue;
+        }
+        public override double Calculate(List<double> inputs) {
+            double solution = 1;
+            foreach(double element in inputs) {
+                solution = solution * element;
+            }
+            return solution;
+        }
+    }
+
+    public class Division : Operation {
+        public Division() {
+            minInputsCount = 2;
+            maxInputsCount = 2;
+        }
+        public override double Calculate(List<double> inputs) {
+            double numerator = inputs[0];
+            double denominator = inputs[1];
+            return numerator / denominator;
+        }
+    }
+
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp {
+    class Program {
+        static void Main(string[] args) {
+
+            while (true) {
+
+                Console.WriteLine("Enter up to 3 numbers separated by a space, then hit Enter");
+
+                string input = Console.ReadLine();
+                //()split and validate user input, and return individual doubles; return to beginning if input invalid
+
+                double[] numbers = ExtractAndValidate(input);
+                if (numbers == null) {
+                    continue;
+                }
+
+                //()check which operations are valid based on the number of inputs
+                List<Operation> validOperations = GetValidOperations(numbers.Length);
+
+                //Ask user to pick one of the valid option, listing the valid operations
+
+                //()check to see if user picked a valid operation, and if they did not ask them to pick again
+                //()perform operation and WriteLine answer
+                //Writeline "Press Enter to begin again"
+
+
+            }
+        }
+
+        private static double[] ExtractAndValidate(string input) {
+            string[] pieces = input.Split(' ', ',');
+            double a;
+            List<double> numbers = new List<double>();
+            foreach(string element in pieces) {
+                bool ok = double.TryParse(element, out a);
+                if (ok == true) {
+                    numbers.Add(a);
+                } 
+                else {
+                    Console.WriteLine("Not a valid entry: " + element);
+                    return null;
+                }
+            }
+            return numbers.ToArray();
+        }
+
+        private static List<Operation> GetValidOperations(int elementsCount) {
+            List<Operation> validOperations = new List<Operation>();
+            foreach (Operation operation in Operation.ALL_OPERATIONS) {
+                if (operation.IsValid(elementsCount)) {
+                    validOperations.Add(operation);
+                }
+            }
+            return validOperations;
+        }
+
+    }
+}
